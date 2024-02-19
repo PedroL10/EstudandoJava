9eaa8cb08b20a2adb464e271bd8588a8bfad2998
PedroLGon <pepluisgon01@gmail.com>
2024-02-19 11:48:49 -0300

    JDBC 4 - Update

--- CursoNelio/Teste/src/application/Program.java
+++ CursoNelio/Teste/src/application/Program.java
@@ -3,10 +3,18 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-	
-		
-		System.out.println("Teste");
 
+		long startTime = System.currentTimeMillis();		
+		
+		for(long i = 0; i <= Integer.MAX_VALUE; i++) {
+			
+			System.out.println(i);
+		}		
+
+		long endTime = System.currentTimeMillis();
+		long elapsedTime = endTime - startTime;
+		System.out.println("Tempo de execução: " + elapsedTime + " milissegundos");
+		
 	}
 
 }
