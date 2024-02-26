f12e87055a095803105522c4362a16b16a23169e
PedroLGon <pepluisgon01@gmail.com>
2024-02-26 15:11:05 -0300

    SellerDao and DepartmentDao

--- CursoNelio/demo-dao-jdbc/src/application/Program.java
+++ CursoNelio/demo-dao-jdbc/src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,9 @@ public class Program {
 	
 		Department d1 = new Department(1, "Books");
 		
+		Seller s1 = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, d1);
+		
 		System.out.println(d1);
+		System.out.println(s1);
 	}
 }
